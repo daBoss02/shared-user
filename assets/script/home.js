c956fb5c83ce6cfcbf198cdac988a4523069e4a7
Seth Vandenbos <seth.vdbos@gmail.com>
2022-12-16 12:30:57 -0600

    randomized people

--- assets/script/home.js
+++ assets/script/home.js
@@ -13,7 +13,7 @@ const followSect = select('.followers');
 textInput.value = '';
 imgInput.value = '';
 
-const url = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
+const url = 'https://randomuser.me/api/?nat=CA&results=10';
 const options = {
   method: 'GET',
   headers: {
