b691128053b932293390dbcafbb13b0a4e1c2384
Seth Vandenbos <seth.vdbos@gmail.com>
2022-12-16 12:25:06 -0600

    with followers

--- assets/script/home.js
+++ assets/script/home.js
@@ -9,6 +9,7 @@ const imgInput = select('.img-input');
 const textInput = select('textarea');
 const postIt = select('.post');
 const area = select('.posts-place')
+const followSect = select('.followers');
 textInput.value = '';
 imgInput.value = '';
 
@@ -21,18 +22,97 @@ const options = {
   mode: 'cors'
 }
 
-const followers = fetch(url, options)
-.then(response => response.json())
-.then(json => json.results)
-.catch(console.error);
+async function getUsers() {
+  try {
+    console.log(url)
+    const result = await fetch(url, options);
 
-console.log(followers)
+    if (result.status >= 200 && result.status < 400) {
+      const data = await result.json();
+      console.log(data)
+      const followers = data.results;
+      console.log(followers)
+      
+
+      createFollower(followers)
+
+    }
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+function createFollower(arr) {
+  arr.forEach(user => {
+    let pic = user.picture.thumbnail;
+    let name = `${user.name.first} ${user.name.last}`
+    let province = user.location.state;
+    switch (province) {
+      case 'Manitoba':
+        province = 'MB';
+        break;
+      case 'Saskatchewan':
+        province = 'SK';
+        break;
+      case 'British Columbia':
+        province = 'BC';
+        break;
+      case 'Alberta':
+        province = 'AB';
+        break;
+      case 'Ontario':
+        province = 'ON';
+        break;
+      case 'New Brunswick':
+        province = 'NB';
+        break;
+      case 'Newfoundland and Labrador':
+        province = 'NL';
+        break;
+      case 'Northwest Territories':
+        province = 'NT';
+        break;
+      case 'Nova Scotia':
+        province = 'NS';
+        break;
+      case 'Nunavut':
+        province = 'NU';
+        break;
+      case 'Prince Edward Island':
+        province = 'PEI';
+        break;
+      case 'Québec':
+        province = 'QC';
+        break;
+      case 'Yukon':
+        province = 'YT';
+        break;
+    }
+    let city = `${user.location.city}, ${province}`
+    addFollower(pic, name, city)
+  })
+}
+
+function addFollower(img, name, location) {
+  let el = create('div');
+  el.classList.add('follower');
+  el.classList.add('flex');
+  let html = `<img class="small" src="${img}" alt="person">
+              <div>
+                <p class="name">${name}</p>
+                <p class="location">${location}</p>
+              </div>`;
+                
+  console.log(html);
+  el.innerHTML = html;
+  followSect.append(el)
+}
+              
+getUsers()
 
 onEvent('change', imgInput, function() {
   label.innerText = imgInput.value.split(/(\\|\/)/g).pop();
 })
-
-
 
 function createText(text) {
   let html = create('div');
